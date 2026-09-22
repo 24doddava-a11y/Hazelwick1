0edd0f1c568dd5cfeb50aad933fc8fe7768d4f21
24doddava-a11y <24doddava@hazelwick.org>
2026-09-22 08:51:36 +0000

    22nd sept 2026

---
+++ python/9B/Cp2/varshika_coding/task4-6.py
@@ -0,0 +1,14 @@
+print()
+print("You can create your own 3 little pigs story. Just complete complete the questions below...")
+
+name1 = input("Enter pig 1's name: ")
+name2 = input("Enter pig 2's name: ")
+name3 = input("Enter pig 3's name: ")
+name4 = input("Enter the wolf's name: ")
+
+material1 = input("Enter a material: ")
+material2 = input("Enter a material: ")
+material3 = input("Enter a material: ")
+
+print()
+print("There once were three little pigs. The first pig was called " + name1 + ", the second was called " + name2 + " and the third was called " + name3 + ". The first house was made from " + material1 + ". The second house was made from " + material2 + ". The third house was made from " + material3 + ". The big bad wolf was called " + name4 + " The big bad wolf blew down the first two houses. He could not destroy the third house made from " + material3 + ". The pigs lived happily ever after.")
