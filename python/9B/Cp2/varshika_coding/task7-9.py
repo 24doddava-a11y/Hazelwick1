b431cddb973d55c653c8d4f008270f176921965c
24doddava-a11y <24doddava@hazelwick.org>
2026-09-23 15:01:23 +0000

    23rd sept 2026

---
+++ python/9B/Cp2/varshika_coding/task7-9.py
@@ -0,0 +1,25 @@
+number = float(input("Enter a number: "))
+number * 1 == m1
+number * 2 == m2
+number * 3 == m3
+number * 4 == m4
+number * 5 == m5
+number * 6 == m6
+number * 7 == m7
+number * 8 == m8
+number * 9 == m9
+number * 10 == m10
+number * 11 == m11
+number * 12 == m12
+print("1 times",number,"=",m1)
+print("2 times",number,"=",m2)
+print("3 times",number,"=",m3)
+print("4 times",number,"=",m4)
+print("5 times",number,"=",m5)
+print("6 times",number,"=",m6)
+print("7 times",number,"=",m7)
+print("8 times",number,"=",m8)
+print("9 times",number,"=",m9)
+print("10 times",number,"=",m10)
+print("11 times",number,"=",m11)
+print("12 times",number,"=",m12)
