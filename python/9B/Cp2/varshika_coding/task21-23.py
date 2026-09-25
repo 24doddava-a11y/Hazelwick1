b45437de378c6af430c6904ece39b1c8c395618f
24doddava-a11y <24doddava@hazelwick.org>
2026-09-25 13:57:27 +0000

    25/09/2026

--- python/9B/Cp2/varshika_coding/task21-23.py
+++ python/9B/Cp2/varshika_coding/task21-23.py
@@ -1,22 +1,22 @@
-q1 = input("12 + 18 =")
+q1 = input("12 + 18 = ")
 if q1 == 30:
     print("you are correct")
 else:
     print("you are not correct.")
 
-q2 = input("18 - 13 =")
+q2 = input("18 - 13 = ")
 if q2 == 5:
     print("you are correct")
 else:
     print("you are not correct.")
 
-q3 = input("25 * 16 =")
+q3 = input("25 * 16 = ")
 if q3 == 400:
     print("you are correct")
 else:
     print("you are not correct.")
 
-q4 = input("365 / 358 =")
+q4 = input("365 / 358 = ")
 if q2 == 1.019553073:
     print("you are correct")
 else:
