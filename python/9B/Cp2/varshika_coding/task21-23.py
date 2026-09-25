7e81571c3e15a767545ddaef5b035785b46d7bd6
24doddava-a11y <24doddava@hazelwick.org>
2026-09-25 16:34:19 +0000

    25th sept 2026

--- python/9B/Cp2/varshika_coding/task21-23.py
+++ python/9B/Cp2/varshika_coding/task21-23.py
@@ -1,14 +1,66 @@
-q1 = input("12 + 18 = ")
-if q1 == 30:
-    print("you are correct")
-else:
-    print("you are not correct.")
+def CheckAnswer(inputEnter , expected):
+    if inputEnter == expected:
+        print("you are correct")
+        return True
+    else:
+        print("you are not correct. Try again.")
+        return False
 
-q2 = input("18 - 13 = ")
-if q2 == 5:
-    print("you are correct")
-else:
-    print("you are not correct.")
+correctAns = 0
+myArray = [1, 2, 3, 4, 5]
+
+answers = [["20 + 10 = ", "30", "Addition"], ["20 - 10 = ", "10"],["20 * 10 = ", "200"], ["20 / 10 = ", "2"], ["200 / 10 = ", "20"]] 
+for val in answers:
+    print("Type of Question=", val[2])
+    q1 = input(val[0])
+    
+    if(CheckAnswer(q1, val[1])):
+        correctAns = correctAns + 1
+
+
+print("correct = ", correctAns, "Out of ", len(answers))
+percentage = (correctAns * 100) / len(answers) 
+
+print("your percentage is", percentage,"%")
+
+
+
+
+
+answers = [["20 + 10 = ", "30"], ["20 - 10 = ", "10"],["20 * 10 = ", "200"], ["20 / 10 = ", "2"]] 
+for val in answers:
+    while(True):
+        q1 = input(val[0])
+        if(CheckAnswer(q1, val[1])):
+            break;
+       
+correctAns = 0
+wrongAns = 0
+answers = [["20 + 10 = ", "30"], ["20 - 10 = ", "10"],["20 * 10 = ", "200"], ["20 / 10 = ", "2"], ["200 / 10 = ", "20"]] 
+for val in answers:
+    q1 = input(val[0])
+    if(CheckAnswer(q1, val[1])):
+        correctAns = correctAns + 1
+    else:
+        wrongAns = wrongAns + 1
+
+print("correct = ", correctAns, "Wrong = ", wrongAns)
+percentage = (correctAns * 100) / (correctAns + wrongAns)
+print("your percentage is", percentage,"%")
+
+
+
+
+
+
+
+while(True):
+    q2 = input("18 - 13 = ")
+    if q2 == "5":
+        print("you are correct")
+        break
+    else:
+        print("you are not correct.")
 
 q3 = input("25 * 16 = ")
 if q3 == 400:
